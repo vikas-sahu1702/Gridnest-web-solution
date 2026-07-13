1b8085cafb03ca594a439d3d50ece463800ff55f
Vishal Sahu <vshalsahu009988@gmail.com>
2026-07-13 23:23:55 +0530

    Add bulk delete contacts endpoint + fix old phone in .env

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -6,6 +6,7 @@ const {
   getContact,
   updateContactStatus,
   deleteContact,
+  deleteAllContacts,
   getContactStats,
 } = require('../controllers/contactController');
 const { contactValidation } = require('../middleware/validate');
@@ -17,6 +18,7 @@ router.post('/', contactLimiter, contactValidation, submitContact);
 
 // Admin routes (protected)
 router.get('/stats', protect, getContactStats);
+router.delete('/bulk/all', protect, deleteAllContacts);
 router.get('/', protect, getContacts);
 router.get('/:id', protect, getContact);
 router.put('/:id', protect, updateContactStatus);
